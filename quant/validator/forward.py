ceffa21b3ec50546b257b608a0bb7010337a5391
Marblez <matthewwang2020@u.northwestern.edu>
2025-08-21 17:29:38 +0900

    Validator Cadence Change

--- quant/validator/forward.py
+++ quant/validator/forward.py
@@ -78,4 +78,4 @@ async def forward(self):
     bt.logging.info(f"Scored responses: {rewards}")
     # Update the scores based on the rewards. You may want to define your own update_scores function for custom behavior.
     self.update_scores(rewards, miner_uids)
-    time.sleep(int(os.getenv("VALIDATOR_CADENCE", 180)))
+    time.sleep(int(os.getenv("VALIDATOR_CADENCE", 500)))
